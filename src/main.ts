e90ad655bfee0d654abab5837126f5c02b583ed2
JChuck0 <110050395+JChuck0@users.noreply.github.com>
2026-08-05 08:17:44 +0000

    Crear la función mostrarSala

--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,53 @@ console.log("Hello from src/main.ts");
 
 export {};
 
+/*Creamos la sala de cine para 8 filas y 10 columnas*/ 
+let filas: number = 8;
+let columnas: number = 10;
+
+const filasReal: number = filas + 1;
+const columnasReal: number = columnas + 1;
+
 
 function crearSala(filas:number , columnas:number): number[][] {
   let cineInicial:number[][] = [];
-  for(let row = 0; row < filas; row++){
+  for(let row = 0; row < filasReal; row++){
     cineInicial[row] = []
-    for(let colum = 0; colum < columnas ; colum++){
+    for(let colum = 0; colum < columnasReal ; colum++){
       cineInicial[row][colum] = 0;
     }
   }
   return cineInicial;
 }
-let prueba = crearSala(5,8)
-console.log(prueba)
+
+/*Función para mostrar la sala de cine*/
+
+function mostrarSala(sala:number[][]): void {
+  let actualSala: string = "";
+  let numeroFila: number = 1;
+  let numeroColumna: number = 1;
+  for(let row = 0; row < sala.length; row++){
+    for(let colum = 0; colum < sala[row].length; colum++){
+      if (row === 0 && colum === 0) {
+        actualSala += "  ";
+      }else if (row === 0) {
+        actualSala += numeroColumna.toString() + " ";
+        numeroColumna++;
+      }else if (colum === 0) {
+        actualSala += numeroFila.toString() + " ";
+        numeroFila++;
+      }else if(sala[row][colum] === 0){
+        actualSala += "L ";
+      }else{
+        actualSala += "X ";
+      }
+    }
+    actualSala += "\n";
+  }
+  console.log(actualSala);
+}
+
+let salaInicial = crearSala(filasReal, columnasReal);
+mostrarSala(salaInicial);
+
+/*Función para reservar un asiento*/
